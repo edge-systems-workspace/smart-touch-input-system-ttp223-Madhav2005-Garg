bf0585ac6546fbb93b58c6505b4f1cf79a780b1c
Madhav2005-Garg <madhavgarg545@gmail.com>
2026-02-20 22:03:43 +0530

    configure digital I/O and serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,16 @@
 
 int touchState = 0;
 
+// ... previous code ...
+
 void setup() {
+    Serial.begin(9600);
+
+    // Configure touch pin as INPUT for digital reading
+    pinMode(TOUCH_PIN, INPUT);
+
+    Serial.println("TTP223 Touch Detection System - Initialized");
+    delay(1000);
 }
 
 void loop() {
